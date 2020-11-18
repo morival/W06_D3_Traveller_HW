89c89c165ca113089ea99f48c40164d31f3ff0c5
Mateusz <mateusz.harlos@gmail.com>
2020-11-18 19:20:23 +0000

    Homework done

---
+++ models/traveller.js
@@ -0,0 +1,30 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function () {
+  return this.journeys.map((journey) => journey.startLocation)
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => journey.endLocation)
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => journey.transport === transport)
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => journey.distance >= minDistance)
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => total += journey.distance,0)
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  return [...new Set(this.journeys.map((journey) => journey.transport))]
+};
+
+
+module.exports = Traveller;
